8edc2386576e4b81f0c4b5bef36702f301f5942b
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:11 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/NoNauseaMod.java
+++ src/main/java/com/peasenet/mod/NoNauseaMod.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoNauseaMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
         GavinsMod.addMod(new ModNoNausea());
     }
 }
